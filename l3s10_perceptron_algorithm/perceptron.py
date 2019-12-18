3fee43e203317fd2d81918069fecb0d5de9303fe
mc <mc.android.developer@gmail.com>
2019-12-17 18:44:15 -0800

    Add data labeling

--- l3s10_perceptron_algorithm/perceptron.py
+++ l3s10_perceptron_algorithm/perceptron.py
@@ -9,6 +9,24 @@ def generate_random_input(X = 10, Y = 2, min = 0, max = 10):
 	print(res)
 	return res
 
+def label_input_data(data):
+	s = data.shape
+	if s[1] != 3:
+		raise Exception('Bad shape ' + str(s) + ' Input data size must be Nx3')
+	
+	for i in data:
+		# Labling logic can be adjusted here
+		score = i[0] * 3 + i[1] * 2 + 5  
+		if score > 2:
+			i[2] = 1
+		else:
+			i[2] = 0
+
+	print('Input data labled: ')
+	print(data)
+	return data
+
+
 class Linear2XPerceptron:
 	def __init__(self, w1 = 1, w2 = 1, b = 0):
 		self.config(w1, w2, b)
@@ -19,22 +37,34 @@ class Linear2XPerceptron:
 		self.W = numpy.array([w1, w2])
 		self.b = b
 
-	def calc2(self, x1, x2):
+	def calc_once(self, x1, x2):
 		return self.w1 * x1 + self.w2 * x2 + self.b
 
 	def calc(self, X):
+		s = X.shape
+		if s[0] != 2:
+			raise Exception('Bad shape ' + str(s) + ' Input data size must be Nx2')
+		
 		return numpy.asscalar(numpy.dot(self.W, X) + self.b)
+		
 
 def main():
   print('Hello Perceptron!')
 
-  x1x2 = generate_random_input()
+  x1x2 = generate_random_input(X=20, Y=3, min=-5, max=5)
+  print()
+  x1x2 = label_input_data(x1x2)
+  print()
 
   p = Linear2XPerceptron()
   p.config(1, 2, 3)
 
   for i in x1x2:
-  	print('For input ' + numpy.array2string(i) + ' perceptron result is ' + str(p.calc(i)))
+  	print('For input ' + numpy.array2string(i[:-1]) + ' perceptron result is ' + str(p.calc(i[:-1])))
+
+  
+
+
 
 if __name__== '__main__':
   main()
