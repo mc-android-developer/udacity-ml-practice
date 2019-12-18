8390ca2f4f1bc0e74e7f92011363805507ad313b
mc <mc.android.developer@gmail.com>
2019-12-17 18:20:58 -0800

    Calculate entire input array on perceptron

--- l3s10_perceptron_algorithm/perceptron.py
+++ l3s10_perceptron_algorithm/perceptron.py
@@ -3,11 +3,11 @@
 import numpy
 import time
 
-def generate_random_input(X = 2, Y = 10, min = 0, max = 10):
-	values = numpy.random.randint(min, max, (X, Y))
+def generate_random_input(X = 10, Y = 2, min = 0, max = 10):
+	res = numpy.random.randint(min, max, (X, Y))
 	print('Random int matrix of size ' + str(X) + 'x' + str(Y) + ' created:')
-	print(values)
-
+	print(res)
+	return res
 
 class Linear2XPerceptron:
 	def __init__(self, w1 = 1, w2 = 1, b = 0):
@@ -19,35 +19,22 @@ class Linear2XPerceptron:
 		self.W = numpy.array([w1, w2])
 		self.b = b
 
-	def calc(self, x1, x2):
+	def calc2(self, x1, x2):
 		return self.w1 * x1 + self.w2 * x2 + self.b
 
-	def calcv(self, X):
-		return numpy.dot(self.W, X) + self.b
-
+	def calc(self, X):
+		return numpy.asscalar(numpy.dot(self.W, X) + self.b)
 
 def main():
   print('Hello Perceptron!')
 
-  generate_random_input()
+  x1x2 = generate_random_input()
 
   p = Linear2XPerceptron()
-
-  res = p.calc(1,3)
-  print(res)
-
-  res = p.calcv(numpy.array([1, 3]))
-  print(res)
-
-
   p.config(1, 2, 3)
 
-  res = p.calc(1,3)
-  print(res)
+  for i in x1x2:
+  	print('For input ' + numpy.array2string(i) + ' perceptron result is ' + str(p.calc(i)))
 
-  res = p.calcv(numpy.array([1, 3]))
-  print(res)
-
-  
 if __name__== '__main__':
   main()
