150ddb66042df567cb42fc4ce1b6c32a60dbd0e3
mc <mc.android.developer@gmail.com>
2019-12-18 16:47:19 -0800

    Add printing input data in visualizable format

--- l3s10_perceptron_algorithm/perceptron.py
+++ l3s10_perceptron_algorithm/perceptron.py
@@ -7,6 +7,7 @@ def generate_random_input(X = 10, Y = 2, min = 0, max = 10):
 	res = numpy.random.randint(min, max, (X, Y))
 	print('Random int matrix of size ' + str(X) + 'x' + str(Y) + ' created:')
 	print(res)
+	print()
 	return res
 
 def label_input_data(data):
@@ -24,8 +25,31 @@ def label_input_data(data):
 
 	print('Input data labled: ')
 	print(data)
+	print()
 	return data
 
+
+def print_visualization_data(data):
+	s = data.shape
+	if s[1] != 3:
+		raise Exception('Bad shape ' + str(s) + ' Input data size must be Nx3')
+
+	str_data_g0 = ''
+	str_data_g1 = ''
+	for i in data:
+		if i[2] == 0:
+			str_data_g0 += '(' + str(i[0]) + ',' + str(i[1]) + '), '
+		elif i[2] == 1:
+			str_data_g1 += '(' + str(i[0]) + ',' + str(i[1]) + '), '
+		else:
+			raise Exception('Unknown label: ' + str(i[2]))
+	
+	print('Visualization data: ')
+	print('(use https://www.desmos.com/calculator to visualize)')
+	print(str_data_g0[:-2])
+	print(str_data_g1[:-2])
+	print()
+	return data
 
 class Linear2XPerceptron:
 	def __init__(self, w1 = 1, w2 = 1, b = 0):
@@ -52,9 +76,8 @@ def main():
   print('Hello Perceptron!')
 
   x1x2 = generate_random_input(X=20, Y=3, min=-5, max=5)
-  print()
-  x1x2 = label_input_data(x1x2)
-  print()
+  label_input_data(x1x2)
+  print_visualization_data(x1x2)
 
   p = Linear2XPerceptron()
   p.config(1, 2, 3)
